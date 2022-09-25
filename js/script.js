3d15ae5bb4f001726f5f709a882798f3fc5e1929
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-09-25 21:29:30 +0300

    added slider

--- js/script.js
+++ js/script.js
@@ -7,3 +7,36 @@ window.onload = function () {
       document.body.classList.remove("loaded_hiding");
    }, 500);
 };
+
+// Плавная прогрузка Элементов
+
+function onEntry(entry) {
+   entry.forEach((change) => {
+      if (change.isIntersecting) {
+         change.target.classList.add("element-show");
+      }
+   });
+}
+
+let options = {
+   threshold: [0.5],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+
+for (let elm of elements) {
+   observer.observe(elm);
+}
+
+// Слайдер
+
+$(".sneakers_slider").slick({
+   infinite: true,
+   slidesToShow: 4,
+   arrows: true,
+   dots: false,
+   //    autoplay: true,
+   //    autoplayspeed: 3000,
+   variableWidth: true,
+
+});
