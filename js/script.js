75fd9294d59f0deff076598cf30bf7579e86b391
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-09-30 21:31:11 +0300

    sending form to telegram finishd :)

--- js/script.js
+++ js/script.js
@@ -192,21 +192,21 @@ window.addEventListener("DOMContentLoaded", function () {
 
 // Отправка сообщений на почту
 
-//E-mail Ajax Send
-$("form").submit(function () {
-   //Change
-   var th = $(this);
-   $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize(),
-   }).done(function () {
-      window.location.href = "pages/thanks.html";
+// //E-mail Ajax Send
+// $("form").submit(function () {
+//    //Change
+//    var th = $(this);
+//    $.ajax({
+//       type: "POST",
+//       url: "mail.php", //Change
+//       data: th.serialize(),
+//    }).done(function () {
+//       window.location.href = "pages/thanks.html";
 
-      setTimeout(function () {
-         // Done Functions
-         th.window.location.href = "../index.html";
-      }, 9000);
-   });
-   return false;
-});
+//       setTimeout(function () {
+//          // Done Functions
+//          th.window.location.href = "../index.html";
+//       }, 9000);
+//    });
+//    return false;
+// });
--- telegram.php
+++ telegram.php
@@ -13,8 +13,8 @@ $size = $_POST['size'];
 $price = $_POST['price'];
 
 
-$token = "1007513873:AAHRuGZ8Cr1wULSsMevQi6S1KtbdwbSyfiU";
-$chat_id = "-399030555";
+$token = "5458501936:AAFNwuGja_j5FJHR2WzsOutvSVQSlEZOo4w";
+$chat_id = "-825651915";
 $arr = array(
   'Имя пользователя: ' => $name,
   'Телефон: ' => $phone,
@@ -30,14 +30,13 @@ $arr = array(
 );
 
 foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
-};
+    $txt .= "<b>".$key."</b> ".$value."%0A";
+  };
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
-
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 if ($sendToTelegram) {
-  header('Location: thanks.html');
-} else {
-  echo "Error";
-}
+    header('Location: thanks.html');
+  } else {
+    echo "Error";
+  }
 ?>
